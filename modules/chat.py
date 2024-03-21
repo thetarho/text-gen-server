777e0e265c3c6921c6994922e46df93714aa2175
TRAIDevOps <seth@thetarho.ai>
2024-03-21 05:10:12 +0000

    Fix TRANSFORMER_CACHE location, fix bug in text-generation-webui code

--- modules/chat.py
+++ modules/chat.py
@@ -633,9 +633,11 @@ def delete_history(unique_id, character, mode):
 
 
 def replace_character_names(text, name1, name2):
-    text = text.replace('{{user}}', name1).replace('{{char}}', name2)
-    return text.replace('<USER>', name1).replace('<BOT>', name2)
-
+    if text:
+        text = text.replace('{{user}}', name1).replace('{{char}}', name2)
+        return text.replace('<USER>', name1).replace('<BOT>', name2)
+    else:
+        return ""
 
 def generate_pfp_cache(character):
     cache_folder = Path(shared.args.disk_cache_dir)
